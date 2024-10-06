f854bb4bc245475417b4f8227160e301f7a0befa
ksm1132 <ksm113283@gmail.com>
2024-10-06 20:53:58 +0900

    SSM追加

--- main.tf
+++ main.tf
@@ -117,3 +117,9 @@ resource "aws_s3_bucket_lifecycle_configuration" "artifact" {
   }
 }
 
+module "ec2_for_ssm_role" {
+  source = "./iam_role"
+  name = "ec2-for-ssm"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.ec2_for_ssm.json
+}
