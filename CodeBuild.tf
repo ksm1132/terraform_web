71dc7d40d0268e820b78ee564c967af0e1b8b59e
ksm1132 <ksm113283@gmail.com>
2024-10-06 20:17:52 +0900

    buildspec.yml修正

--- CodeBuild.tf
+++ CodeBuild.tf
@@ -46,25 +46,3 @@ resource "aws_codebuild_project" "example" {
   }
 }
 
-data "aws_iam_policy_document" "codepipeline" {
-  statement {
-    effect = "Allow"
-    resources = ["*"]
-
-    actions = [
-      "s3:PutObject",
-      "s3:GetObject",
-      "s3:GetObjectVersion",
-      "s3:GetBucketVersioning",
-      "codebuild:BatchGetBuilds",
-      "codebuild:StartBuild",
-      "ecs:DescribeServices",
-      "ecs:DescribeTaskDefinition",
-      "ecs:DescribeTasks",
-      "ecs:ListTasks",
-      "ecs:RegisterTaskDefinition",
-      "ecs:UpdateService",
-      "iam:PassRole",
-    ]
-  }
-}
---
+++ SSH.tf
@@ -0,0 +1,3 @@
+# data "aws_iam_policy_document" "ec2_for_ssm" {
+#   source_json = data.aws_iam_policy.ec2_for_ssm.policy
+# }
